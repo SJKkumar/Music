0468b4d7d0126083a32aba8178ce8a53c8daca50
Kishore Kumar S <92592762+SJKkumar@users.noreply.github.com>
2024-08-19 21:09:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audio-player');
     const songList = document.getElementById('song-list');
 
-    // List of music files
     const songs = [
         { title: 'Makkamishi', url: 'https://raw.githubusercontent.com/SJKkumar/Music/main/songs/Makkamishi.mp3' },
-        // Add more songs here if needed
+        // Add more songs here
     ];
 
-    // Populate song list
     songs.forEach(song => {
         const li = document.createElement('li');
         li.textContent = song.title;
